7bc78422a1c60685f1e1a5f50389c9513c9ccb34
Johnse Chance <jchance.is2cool@gmail.com>
2023-06-13 22:29:21 -0400

    finished message protocol

--- src/protocol.nim
+++ src/protocol.nim
@@ -1,9 +1,27 @@
-
+import json
 type
     Message* = object
         username*: string
         message*: string
 
 proc parseMessage*(data: string): Message = 
-    discard
+    let dataJson = parseJson(data)
+    result.username = dataJson["username"].getStr()
+    result.message = dataJson["message"].getStr()
 
+proc createMessage*(username, message: string): string =
+    result = $(%{
+        "username": %username,
+        "message": %message
+    }) & "\c\l"
+
+when isMainModule:
+    block:
+        let data = """{"username": "John", "message": "Hi!"}"""
+        let parsed = parseMessage(data)
+        doAssert parsed.username == "John"
+        doAssert parsed.message == "Hi!"
+    block:
+        let expected = """{"username":"dom","message":"hello"}""" & "\c\l"
+        doAssert createMessage("dom", "hello") == expected
+    echo("All tests passed!")
